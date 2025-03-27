06300bf92d55cd885d42f4dfc5fdf14446d7a3a9
Karuna Issa <karunaissa400@gmail.com>
2025-03-27 19:17:17 +0530

    Update test_sample.py

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,17 +1,2 @@
-#tests/tests_sample.py
-import math
-
-num1 = 34
-num2 = 36
-sign = "-"
-if sign == "+":
-  result = num1 + num2 
-  print (result)
-elif sign == "-":
-  result = num1 - num2
-  print (result)
-elif sign == "*":
-  result = num1 * num2
-  print (result)
-else:
-  print (f'unknown: {sign} is not valid'}
+def test_example():
+    assert 1 + 1 == 2
