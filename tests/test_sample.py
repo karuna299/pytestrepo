e8edc9c94a80f2d2d59be85010321a598ccce213
Karuna Issa <karunaissa400@gmail.com>
2025-03-27 19:15:04 +0530

    Create test_sample.py

---
+++ tests/test_sample.py
@@ -0,0 +1,17 @@
+#tests/tests_sample.py
+import math
+
+num1 = 34
+num2 = 36
+sign = "-"
+if sign == "+":
+  result = num1 + num2 
+  print (result)
+elif sign == "-":
+  result = num1 - num2
+  print (result)
+elif sign == "*":
+  result = num1 * num2
+  print (result)
+else:
+  print (f'unknown: {sign} is not valid'}
